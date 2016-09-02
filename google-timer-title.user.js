0cbf792666a53139edeb22cd3a51096f60a2a63a
Art <a.molcanovas@gmail.com>
2016-09-02 11:52:31 +0100

    changed favicon switcher to a try-catch

--- google-timer-title.user.js
+++ google-timer-title.user.js
@@ -4,7 +4,7 @@
 // @author         Alorel <a.molcanovas@gmail.com>
 // @description    Automatically updates the title when using Google's timer
 // @include        https://*google.*/search?*
-// @version        1.0
+// @version        1.0.2
 // @icon           https://cdn.rawgit.com/AlorelUserscripts/google-timer-title-switcher/master/icon.png
 // @run-at         document-end
 // @grant          GM_info
@@ -12,7 +12,7 @@
 // @downloadURL    https://raw.githubusercontent.com/AlorelUserscripts/google-timer-title-switcher/master/google-timer-title.user.js
 // ==/UserScript==
 
-(function (document, MutationObserver) {
+(function () {
     var timerArea,
         container,
         title;
@@ -21,13 +21,10 @@
         timerArea = container.querySelector("div");
         title = document.querySelector("head>title");
 
-        (function () {
-            var faviconElement;
-            if (faviconElement = document.querySelector('link[rel="shortcut icon"]')) {
-                faviconElement.setAttribute("href", GM_info.script.icon);
-                faviconElement = null;
-            }
-        })();
+        try {
+            document.querySelector('link[rel="shortcut icon"]').setAttribute("href", GM_info.script.icon);
+        } catch (e) {
+        }
 
         (new MutationObserver(function () {
             if (container.classList.contains("act-tim-paused")) {
@@ -44,4 +41,4 @@
             subtree: true
         });
     }
-})(document, MutationObserver);
+})();
